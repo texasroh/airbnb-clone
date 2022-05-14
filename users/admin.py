eb3e39695867e73b5c3620d0277048e475de6eb2
texasroh <texasroh@gmail.com>
2022-05-14 03:28:09 -0400

    admin panel create user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    """Custom User Admin"""
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )
+
+
+# admin.site.register(models.User, CustomUserAdmin)
