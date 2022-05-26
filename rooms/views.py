f2719e3cdc99fc10440bb46bcc7222609258e6b5
June Roh <june.roh@streetsmarket.com>
2022-05-26 13:56:13 -0400

    filter rooms

--- rooms/views.py
+++ rooms/views.py
@@ -72,7 +72,7 @@ def search(request):
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
     instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    superhost = request.GET.get("superhost", False)
 
     form = {
         "city": city,
@@ -86,7 +86,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
     }
 
     room_types = models.RoomType.objects.all()
@@ -109,6 +109,33 @@ def search(request):
 
     if room_type != 0:
         filter_args["room_type__pk"] = room_type
+
+    if price > 0:
+        filter_args["price__lte"] = price
+
+    if guests > 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms > 0:
+        filter_args["bedrooms__gte"] = bedrooms
+    if beds > 0:
+        filter_args["beds__gte"] = beds
+    if baths > 0:
+        filter_args["baths__gte"] = baths
+
+    if instant:
+        filter_args["instant_book"] = True
+
+    if superhost:
+        filter_args["host__superhost"] = True
+
+    if s_amenities:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = s_amenity
+
+    if s_facilities:
+        for s_facility in s_facilities:
+            filter_args["facilitires__pk"] = s_facility
 
     rooms = models.Room.objects.filter(**filter_args)
 
