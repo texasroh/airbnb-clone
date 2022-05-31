81066eeb2de0dd1d897f5d699faa50b1e551beec
June Roh <june.roh@streetsmarket.com>
2022-05-31 15:35:52 -0400

    login with loginview CBV

---
+++ users/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from . import models
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     try:
+    #         models.User.objects.get(username=email)
+    #     except models.User.DoesNotExist:
+    #         raise forms.ValidationError("User does not exist")
+    #     return email
+
+    # def clean_password(self):
+    #     email = self.cleaned_data.get("email")
+    #     password = self.cleaned_data.get("password")
+    #     try:
+    #         user = models.User.objects.get(username=email)
+    #         if user.check_password(password):
+    #             pass
+    #         else:
+    #             raise forms.ValidationError("Password is wrong")
+    #     except models.User.DoesNotExist:
+    #         pass
+
+    #     return password
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(username=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                # raise forms.ValidationError("Password is wrong")
+                self.add_error("password", forms.ValidationError("Password is wrong"))
+        except models.User.DoesNotExist:
+            # raise forms.ValidationError("User does not exist")
+            self.add_error("email", forms.ValidationError("User does not exist"))
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
+class LoginView(View):
+
+    """LoginView Definitions"""
+
+    def get(self, request):
+        form = forms.LoginForm(initial={"email": "test@test.com"})
+
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
+
+
+# def login_view(request):
+#     if request.method == "GET":
+#         pass
+#     elif request.method == "POST":
+#         pass
