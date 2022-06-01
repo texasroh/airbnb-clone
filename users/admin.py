3c4b23b2aafe9a7afbc86d5d675e7de8342d9a70
June Roh <june.roh@streetsmarket.com>
2022-06-01 17:16:50 -0400

    github oauth

--- users/admin.py
+++ users/admin.py
@@ -20,6 +20,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -39,6 +40,7 @@ class CustomUserAdmin(UserAdmin):
         "is_superuser",
         "email_verified",
         "email_secret",
+        "login_method",
     )
 
 
