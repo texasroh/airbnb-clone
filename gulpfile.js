e389316a43a1847e2e257acf2d2e5b5613e9b95c
texasroh <texasroh@gmail.com>
2022-06-02 22:49:45 -0400

    gulp error fixed

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ const css = () => {
     const sass = require('gulp-sass')(require('sass'));
     const minify = require('gulp-csso');
     sass.compiler = require('node-sass');
-    return gulp.src('assets/scss/styles.scss')
+    return gulp.src('assets/scss/styles.scss', { allowEmpty: true })
         .pipe(sass().on('error', sass.logError))
         .pipe(postCSS([require('tailwindcss'), require('autoprefixer')]))
         .pipe(minify())
-        .pipe(gulp.dest('static/css'));
+        .pipe(gulp.dest("static/css"));
 };
 
 exports.default = css;
