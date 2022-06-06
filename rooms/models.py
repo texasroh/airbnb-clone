84dfa247a805575fd16cc35f9312f911b32fbe9a
texasroh <texasroh@gmail.com>
2022-06-06 03:12:03 -0400

    signup login social login css

--- rooms/models.py
+++ rooms/models.py
@@ -105,3 +105,7 @@ class Room(core_models.TimeStampModel):
         if len(all_reviews) == 0:
             return 0
         return round(all_ratings / len(all_reviews), 2)
+
+    def first_photo(self):
+        photo = self.photos.all()[1]
+        return photo.file.url
