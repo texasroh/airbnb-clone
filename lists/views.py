39223032c008e4004a8bc20f2d4940efa71982a8
June Roh <texasroh@gmail.com>
2022-06-13 02:17:56 -0400

    add and remove favorite

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from rooms import models as room_models
+from . import models
 
-# Create your views here.
+
+def toggle_room(request, room_pk):
+    action = request.GET.get("action", None)
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
+    if room and action:
+        the_list, _ = models.List.objects.get_or_create(
+            user=request.user, name="My Favorites Houses"
+        )
+        if action == "add":
+            the_list.rooms.add(room)
+        elif action == "remove":
+            the_list.rooms.remove(room)
+
+    return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
